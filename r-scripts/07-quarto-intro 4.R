768c85d0711f68f440307f35a71fb0a36e327f40
ttalVlatt <97192696+ttalVlatt@users.noreply.github.com>
2024-01-08 22:39:58 -0500

    Built site for gh-pages

---
+++ r-scripts/07-quarto-intro 4.R
@@ -0,0 +1,26 @@
+knitr::opts_chunk$set(echo = TRUE)
+
+summary(cars)
+
+plot(pressure)
+
+#writeLines(readLines("../scripts/test_scores.Rmd")[2:7])
+
+## this is active R code
+## when compiled: the R code is run, and both code and results are printed
+x <- rnorm(1000)
+x
+
+#writeLines(readLines("../scripts/test_scores.Rmd")[9:48])
+
+#writeLines(readLines("../scripts/test_scores.Rmd")[9])
+
+knitr::opts_chunk$set(echo = TRUE)
+
+#writeLines(readLines("../scripts/test_scores.Rmd")[50:78])
+
+#writeLines(readLines("../scripts/test_scores.Rmd")[69:78])
+
+#writeLines(readLines("../scripts/test_scores.Rmd")[89:126])
+
+#writeLines(readLines("../scripts/test_scores.Rmd")[129:158])
