dd23209b16e6b55d0517dc7ba0b3b824bfb52603
ttalVlatt <97192696+ttalVlatt@users.noreply.github.com>
2024-01-13 13:29:20 -0500

    Built site for gh-pages

--- r-scripts/03-wrangle-i.R
+++ r-scripts/03-wrangle-i.R
@@ -4,8 +4,10 @@
 ## <FILE> dw_one.R 
 ## <INIT> 20 January 2020
 ## <AUTH> Benjamin Skinner (GitHub/Twitter: @btskinner)
+## <MODI> Matt Capaldi on 12 Jan 2024
 ##
 ################################################################################
+
 
 ## ---------------------------
 ## libraries
@@ -24,130 +26,134 @@ library(tidyverse)
 ## data are CSV, so we use read_csv() from the readr library
 df <- read_csv(file.path("data", "hsls-small.csv"))
 
-## ---------------------------
-## process
-## ---------------------------
+## Without |>
+select(df, x1txmtscor)
 
-## -----------------
-## select
-## -----------------
+## With |>
+df |> select(x1txmtscor)
 
-## select columns we need and assign to new object
-df_tmp <- select(df, stu_id, x1stuedexpct, x1paredexpct, x1region)
+## Without |>
+filter(select(df, x1txmtscor), x1txmtscor > 50)
 
-## show
-df_tmp
+## With |>
+df |> select(x1txmtscor) |> filter(x1txmtscor > 50)
+
+## Without |>
+mutate(filter(select(df, x1txmtscor), x1txmtscor > 50), square_root = sqrt(x1txmtscor))
+
+## With |>
+df |> select(x1txmtscor) |> filter(x1txmtscor > 50) |> mutate(square_root = sqrt(x1txmtscor))
+
+df |>
+  select(x1txmtscor) |>
+  filter(x1txmtscor > 50) |>
+  mutate(square_root = sqrt(x1txmtscor))
+
+## Without the |>, we could technically break it down step by step assigning
+## after each step, but again it's confusing 
+temp <- select(df, x1txmtscor)
+temp <- filter(temp, x1txmtscor > 50)
+temp <- mutate(temp, square_root = sqrt(x1txmtscor))
+temp
+
+df_backward_pass <- df |>
+  select(x1txmtscor) |>
+  filter(x1txmtscor > 50) |>
+  mutate(square_root = sqrt(x1txmtscor))
+
+df |>
+  select(x1txmtscor) |>
+  filter(x1txmtscor > 50) |>
+  mutate(square_root = sqrt(x1txmtscor)) ->
+  df_forward_pass
+
+identical(df_backward_pass, df_forward_pass)
+
+df |> select(stu_id, x1stuedexpct, x1paredexpct, x1region)
+
+df_small <- df |> select(stu_id, x1stuedexpct, x1paredexpct, x1region)
+
+df |> mutate(square_root = sqrt(x1txmtscor))
 
 ## -----------------
 ## mutate
 ## -----------------
 
 ## see unique values for student expectation
-count(df_tmp, x1stuedexpct)
+df_small |> count(x1stuedexpct)
 
 ## see unique values for parental expectation
-count(df_tmp, x1paredexpct)
+df_small |> count(x1paredexpct)
 
-## use case_when to overwrite -8 and 11 with NA in our two expectation variables
-df_tmp <- mutate(df_tmp,
-                 ## correct student expectations 
-                 x1stuedexpct = ifelse(x1stuedexpct < 0,   # is value < 0?
-                                       NA,                 # T: replace with NA
-                                       x1stuedexpct),      # F: replace with self
-                 x1stuedexpct = ifelse(x1stuedexpct == 11, # is value == 11?
-                                       NA,                 # T: replace with NA
-                                       x1stuedexpct),      # F: replace with self
-                 ## correct parental expectations
-                 x1paredexpct = ifelse(x1paredexpct < 0,   # (same as above...)
-                                       NA,                 
-                                       x1paredexpct),       
-                 x1paredexpct = ifelse(x1paredexpct == 11,
-                                       NA,
-                                       x1paredexpct))
+df_small <- df_small |>
+  mutate(student_exp = ifelse(x1stuedexpct == -8, NA, x1stuedexpct))
 
-## again see unique values for student expectation
-count(df_tmp, x1stuedexpct)
+print(df_small, n = 26)
 
-## again see unique values for parental expectation
-count(df_tmp, x1paredexpct)
+df_small <- df_small |>
+  mutate(student_exp = ifelse(x1stuedexpct %in% list(-8, -9, 11), NA, x1stuedexpct),
+         parent_exp = ifelse(x1paredexpct %in% list(-8, -9, 11), NA, x1paredexpct))
 
-## mutate (notice that we use df_tmp now)
-df_tmp <- mutate(df_tmp,
-                 high_expct = ifelse(x1stuedexpct > x1paredexpct, # test
-                                     x1stuedexpct,                # if TRUE
-                                     x1paredexpct))               # if FALSE
+print(df_small, n = 26)
 
-## show
-df_tmp
+df_small |> count(student_exp) 
+df_small |> count(parent_exp)
 
-## correct for NA values
-df_tmp <- mutate(df_tmp,
-                 ## step 1: compare with student's expectations
-                 high_expct = ifelse(is.na(high_expct) & !is.na(x1stuedexpct), 
-                                     x1stuedexpct,                
-                                     high_expct),
-                 ## step 2: compare with parent's expectations
-                 high_expct = ifelse(is.na(high_expct) & !is.na(x1paredexpct), 
-                                     x1paredexpct,                
-                                     high_expct))               
+df_small <- df_small |>
+  mutate(high_exp = ifelse(student_exp > parent_exp, student_exp, parent_exp))
 
-## show
-df_tmp
+print(df_small, n = 26)
+
+mean(c(5, 6, 4, NA))
+
+mean(c(5, 6, 4, NA), na.rm = T)
+
+df_small <- df_small |>
+  mutate(high_exp = ifelse(is.na(high_exp) & !is.na(student_exp), student_exp, high_exp),
+         high_exp = ifelse(is.na(high_exp) & !is.na(parent_exp), parent_exp, high_exp))
+
+print(df_small, n = 26)
 
 ## -----------------
 ## filter
 ## -----------------
 
 ## get summary of our new variable
-count(df_tmp, high_expct)
+df_small |> count(high_exp)
 
 ## filter out missing values
-df_tmp <- filter(df_tmp, !is.na(high_expct))
+df_small_cut <- df_small |> filter(!is.na(high_exp))
 
-## show
-df_tmp
+df_small_cut |> count(high_exp)
 
-## is the original # of rows - current # or rows == NA in count?
-nrow(df) - nrow(df_tmp)
+## does the original # of rows - current # or rows == NA in count?
+nrow(df_small) - nrow(df_small_cut)
 
 ## -----------------
 ## summarize
 ## -----------------
 
 ## get average (without storing)
-summarize(df_tmp, high_expct_mean = mean(high_expct))
+df_small_cut |> summarize(mean(high_exp))
 
-## check our census regions
-count(df_tmp, x1region)
+df_small_cut |> summarize(mean_exp = mean(high_exp))
 
-## get expectations average within region
-df_tmp <- group_by(df_tmp, x1region)
-
-## show grouping
-df_tmp
-
-## get average (assigning this time)
-df_tmp <- summarize(df_tmp, high_expct_mean = mean(high_expct))
-
-## show
-df_tmp
-
-## -----------------
-## arrange
-## -----------------
-
-## arrange from highest expectations (first row) to lowest
-df_tmp <- arrange(df_tmp, desc(high_expct_mean))
-
-## show
-df_tmp
+## get grouped average
+df_small_cut |>
+  group_by(x1region) |>
+  summarize(mean_exp = mean(high_exp))
 
 ## ---------------------------
 ## output
 ## ---------------------------
 
 ## write with useful name
-write_csv(df_tmp, file.path("data", "high_expct_mean_region.csv"))
+
+df_small_cut |>
+  group_by(x1region) |>
+  summarize(mean_exp = mean(high_exp)) |>
+  write_csv(file.path("data", "region-expects.csv"))
+
 
 ## ---------------------------
 ## appendix
@@ -156,49 +162,34 @@ write_csv(df_tmp, file.path("data", "high_expct_mean_region.csv"))
 ## Let's redo the analysis above, but with a fully chained set of
 ## functions.
 
+## start with original df
+df |>
+  ## select columns we want
+  select(stu_id, x1stuedexpct, x1paredexpct, x1region) |>
+  ## If expectation is -8, -9. or 11, make it NA
+  mutate(student_exp = ifelse(x1stuedexpct %in% list(-8, -9, 11), NA, x1stuedexpct),
+         parent_exp = ifelse(x1paredexpct %in% list(-8, -9, 11), NA, x1paredexpct)) |>
+  ## Make a new variable called high_exp that is the higher or parent and student exp
+  mutate(high_exp = ifelse(student_exp > parent_exp, student_exp, parent_exp)) |>
+  ## If one exp is NA but the other isn't, keep the value not the NA
+  mutate(high_exp = ifelse(is.na(high_exp) & !is.na(student_exp), student_exp, high_exp),
+         high_exp = ifelse(is.na(high_exp) & !is.na(parent_exp), parent_exp, high_exp)) |>
+  ## Drop is high_exp is still NA (neither parent or student answereed)
+  filter(!is.na(high_exp)) |>
+  ## Group the results by region
+  group_by(x1region) |>
+  ## Get the mean of high_exp (by region)
+  summarize(mean_exp = mean(high_exp)) |>
+  ## Write that to a .csv file
+  write_csv(file.path("data", "region-expects-chain.csv"))
+  
 
-## start with the original data frame...
-df_tmp_chained <- df |>
-    ## (df is piped in): select the columns we want
-    select(stu_id, x1stuedexpct, x1paredexpct, x1region) |>
-    ## (selected df is piped in): mutate our data, starting with missing
-    mutate(x1stuedexpct = ifelse(x1stuedexpct < 0,   
-                                 NA,                 
-                                 x1stuedexpct),      
-           x1stuedexpct = ifelse(x1stuedexpct == 11, 
-                                 NA,                 
-                                 x1stuedexpct),      
-           x1paredexpct = ifelse(x1paredexpct < 0,   
-                                 NA,                 
-                                 x1paredexpct),       
-           x1paredexpct = ifelse(x1paredexpct == 11,
-                                 NA,
-                                 x1paredexpct),
-           ## create new column
-           high_expct = ifelse(x1stuedexpct > x1paredexpct, 
-                               x1stuedexpct,                
-                               x1paredexpct),
-           ## fix new column NAs
-           high_expct = ifelse(is.na(high_expct) & !is.na(x1stuedexpct), 
-                               x1stuedexpct,                
-                               high_expct),
-           high_expct = ifelse(is.na(high_expct) & !is.na(x1paredexpct), 
-                               x1paredexpct,                
-                               high_expct)) |>
-    ## (mutated df is piped in): filter in non-missing rows
-    filter(!is.na(high_expct)) |>
-    ## (filtered df is piped in): group by region
-    group_by(x1region) |>
-    ## (grouped df is piped in): summarize mean expectations
-    summarize(high_expct_mean = mean(high_expct)) |>
-    ## (summarized df is piped in): arrange average expectations hi --> lo
-    arrange(desc(high_expct_mean))         
 
-## show
-df_tmp_chained
-
-## test using identical()
-identical(df_tmp, df_tmp_chained)
+non_chain <- read_csv(file.path("data", "region-expects.csv"))
+chain <- read_csv(file.path("data", "region-expects-chain.csv"))
+ 
+all.equal(non_chain, chain)
+      
 
 ## -----------------------------------------------------------------------------
 ## end script
