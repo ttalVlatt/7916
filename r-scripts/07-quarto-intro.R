d6223d75ab7cb2a9a7ac4eae04679b1750b810cf
ttalVlatt <matt.capaldi@outlook.com>
2024-02-27 16:16:52 -0500

    Built site for gh-pages

--- r-scripts/07-quarto-intro.R
+++ r-scripts/07-quarto-intro.R
@@ -20,31 +20,3 @@ patch
 
 
 print(df_long)
-
-## Read in using .dta so we have nice labels
-df <- haven::read_dta("data/hsls-small.dta") |>
-  drop_na(x1txmtscor)
-
-df |>
-  summarize(mean(x1txmtscor))
-
-library(knitr)
-
-df |>
-  summarize(mean(x1txmtscor)) |>
-  kable()
-
-df |>
-  summarize(mean(x1txmtscor)) |>
-  kable(col.names = c("Mean of Math Score"),
-        digits = 2)
-
-df |> 
-  group_by(as_factor(x1region), as_factor(x1sex)) |>
-  summarize(mean = mean(x1txmtscor),
-            median = median(x1txmtscor),
-            min = min(x1txmtscor),
-            max = max(x1txmtscor)) |>
-  kable(col.names = c("Region", "Sex", "Mean", "Median", "Min", "Max"),
-        digits = 2,
-        caption = "Math Score by Region and Sex")
