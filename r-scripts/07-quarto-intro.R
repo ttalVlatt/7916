7233c15c29230c5247bf334c2ffce5bb92e2f1fa
ttalVlatt <matt.capaldi@outlook.com>
2025-02-25 00:56:59 -0500

    Built site for gh-pages

--- r-scripts/07-quarto-intro.R
+++ r-scripts/07-quarto-intro.R
@@ -1,67 +1,68 @@
-# ## You should have these packages already, we installed them our first lesson
-# ## but if you're not sure, run this code just in case
-# install.packages(c("knitr", "rmarkdown", "quarto"))
-
 library(tidyverse)
-library(gtsummary)
+library(haven)
 
-df <- read_csv(file.path("data", "hsls-small.csv"))
+data <- read_dta("data/hsls-small.dta")
 
-ggplot(data = df) +
-  geom_histogram(mapping = aes(x = x1txmtscor))
-
-
+ggplot(data) +
+  geom_histogram(aes(x = x1txmtscor))
 
 
 
-# source("06-viz-ii.R")
+## ggsave("math-scores.png")
 
-patch
+## install.packages("knitr")
 
-
-
-print(data_long)
-
-## Read in using .dta so we have nice labels
-df <- haven::read_dta("data/hsls-small.dta") |>
+data <- data |> 
   drop_na(x1txmtscor)
 
-df |>
+data |>
   summarize(mean(x1txmtscor))
 
 library(knitr)
 
-df |>
+data |>
   summarize(mean(x1txmtscor)) |>
   kable()
 
-df |>
+data |>
   summarize(mean(x1txmtscor)) |>
   kable(col.names = c("Mean of Math Score"),
         digits = 2)
 
-df |> 
-  group_by(as_factor(x1region), as_factor(x1sex)) |>
+data |> 
+  group_by(as_factor(x1region)) |>
   summarize(mean = mean(x1txmtscor),
             median = median(x1txmtscor),
             min = min(x1txmtscor),
             max = max(x1txmtscor)) |>
-  kable(col.names = c("Region", "Sex", "Mean", "Median", "Min", "Max"),
-        digits = 2,
-        caption = "Math Score by Region and Sex")
+  kable(col.names = c("Region", "Mean", "Median", "Min", "Max"),
+        digits = 2)
 
-df |>
+
+
+
+
+## source("lesson-06-viz-ii.R")
+
+patch
+
+## install.packages("gtsummary")
+## library(gtsummary)
+
+library(gtsummary)
+
+data |>
   select(x1txmtscor, x1region) |>
   tbl_summary()
 
-df |>
+data |>
   select(x1txmtscor, x1region) |>
   tbl_summary(type = all_continuous() ~ "continuous2",
               statistic = c(all_continuous() ~ c("{mean}",
                                                  "{sd}",
                                                  "{min} to {max}")))
 
-df |>
+data |>
   select(x1txmtscor, x1region) |>
   tbl_summary(type = all_continuous() ~ "continuous2",
               statistic = c(all_continuous() ~ c("{mean}",
