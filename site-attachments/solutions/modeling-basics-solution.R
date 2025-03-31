6de1614b1b1ff9a194843c749bb3cd2e43d51b51
juewu-wj <juewu0221@gmail.com>
2025-03-31 17:13:18 -0400

    Updated Content for Spring2025

--- site-attachments/solutions/modeling-basics-solution.R
+++ site-attachments/solutions/modeling-basics-solution.R
@@ -18,11 +18,47 @@ library(tidyverse)
 ## ---------------------------
 ##' [Q1]
 ## ---------------------------
+data <- read_csv("data/hsls-small.csv") |> 
+  select(stu_id, x1sex, x1txmtscor, x1paredu, x1poverty185, x4evratndclg, x1famincome)
 
+data <- data |>
+  filter(! if_any(.cols = everything(),
+                  .fns = ~ . %in% c(-8, -9)))
+
+data <- data |>
+  mutate(across(.cols = c(stu_id, x1sex, x1paredu, x1poverty185, x1famincome),
+                .fns = ~ factor(.)))
+
+# 1a
+t.test(x1txmtscor ~ x1poverty185, data = data)
+
+# 1b
+ggplot(data, aes(x = x1poverty185, y = x1txmtscor, fill = x1poverty185)) +
+  geom_boxplot() +
+  labs(x = "Poverty Status", 
+       y = "Math Score", 
+       fill = "Poverty Level") 
 
 ## ---------------------------
 ##' [Q2]
 ## ---------------------------
+
+# 2a
+logit_reg <- glm(x4evratndclg ~ x1txmtscor + x1sex + x1famincome + x1paredu, data = data)
+summary(logit_reg)
+
+# 2b
+# stargazer
+stargazer(logit_reg, type = "html")
+
+# gtsummary
+tbl_regression(logit_reg,
+               label = list(x1txmtscor ~ "Math Score",
+                            x1sex ~ "Sex",
+                            x1famincome ~ "Family Income",
+                            x1paredu ~ "Parental Education")) |>
+  add_significance_stars(hide_ci = FALSE, hide_p = FALSE) |>
+  modify_column_unhide(std.error)
 
 ## -----------------------------------------------------------------------------
 ##' *END SCRIPT*
